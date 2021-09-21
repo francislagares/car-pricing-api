0d038f2982407a187e43a164a08e0e5d1a6b6cfb
Francis Lagares <francis.lagares@gmail.com>
2021-09-21 19:41:37 +0200

    feat: create auth service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [PrismaService],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, AuthService, PrismaService],
 })
 export class UsersModule {}
