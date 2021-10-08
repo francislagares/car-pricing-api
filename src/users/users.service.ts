edb94ee6dfb49e8a387496aeb15f85c595822c33
Francis Lagares <francis.lagares@gmail.com>
2021-10-08 19:47:59 +0200

    test(users): write auth service spec

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthUserDto } from './dto/auth-user.dto';
 import { GetUsersFilterDto } from './dto/get-users-filter.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
